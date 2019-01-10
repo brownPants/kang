a7c0ef51de7f5fbac5156ace670b7e9858cccf9d
Administrator <Administrator@MSDN-SPECIAL>
2019-01-10 16:16:12 +0900

    [강승우] 프로젝트 생성

---
+++ src/main/java/com/seungwoo/kang/KangApplication.java
@@ -0,0 +1,14 @@
+package com.seungwoo.kang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KangApplication.class, args);
+	}
+
+}
+
